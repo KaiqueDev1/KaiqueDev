87b3fa9029e2368a4c08fee8bc0aefb4638358dc
kaiqueZpriv1 <kakamelo141205@gmail.com>
2023-04-08 20:27:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-open").addEventListener("click", function() {
+  document.getElementById("menu").style.width = "60%"
+})
+
+document.getElementById("btn-close").addEventListener("click", function() {
+  document.getElementById("menu").style.width = "0"
+})

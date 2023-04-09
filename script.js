93ff5a6927912c99290991c16ed28f9ec91456c5
kaiqueZpriv1 <kakamelo141205@gmail.com>
2023-04-09 18:28:34 -0300

    link menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-document.getElementById("btn-open").addEventListener("click", function() {
-  document.getElementById("menu").style.width = "60%"
-})
+document.getElementById("btn-open").addEventListener("click", function () {
+  document.getElementById("menu").style.width = "60%";
+});
 
-document.getElementById("btn-close").addEventListener("click", function() {
-  document.getElementById("menu").style.width = "0"
-})
+document.getElementById("btn-close").addEventListener("click", function () {
+  document.getElementById("menu").style.width = "0";
+});
+
+document.querySelector(".link-mobile").addEventListener("click", function () {
+  document.getElementById("menu").style.width = "0";
+});
